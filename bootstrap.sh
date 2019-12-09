f937a3d5eacf8258ca4615476a912c0e8501072b
Ayushman Khazanchi <ayushman.khazanchi@oracle.com>
2019-12-09 16:40:40 +0530

    draft - restructing ansible and moving stuff around

---
+++ bootstrap.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+#--------------------------------------------------------
+# Prerequisite files: hosts, .gitignore
+# TODO: Clean up ansible into role/setup
+# TODO: Create hooks for packages/ansible vs clean setup
+#--------------------------------------------------------
+
+# Start
+echo "--- Starting clean setup --- "
+mkdir -p ~/workspace/setup
+
+# Get the repo
+cd ~/workspace
+git clone git@github.com:aykhazanchi/setup-mac.git .
+
+# Install xcode
+xcode-select --install
+
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install pip3 and python3 from brew
+brew install python3
+brew install pip3
+
+pip3 install --upgrade pip
+pip3 install --user ansible
+pip3 install --upgrade setuptools
+pip3 install virtualenv
+pip3 install virtualenvwrapper
+
+# Run ansible to setup mac
+cd ~/workspace/setup-mac
+ansible-playbook -i hosts setup-mac.yaml --verbose
